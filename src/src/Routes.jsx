c1a7d8235df85984b29d6b145ac57b43ee3f8f9e
Fabien <fabien.webdev@gmail.com>
2020-09-12 09:14:04 +0900

    add boilerplate

---
+++ src/src/Routes.jsx
@@ -0,0 +1,10 @@
+import React, { memo } from 'react'
+// import PropTypes from 'prop-types'
+
+const Routes = (props) => {
+  return <div>Routes</div>
+}
+
+export default memo(Routes)
+
+Routes.propTypes = {}
